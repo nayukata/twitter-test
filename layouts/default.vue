0df9293c91dde563b81af865259fa3ce64586d0d
nayukata <natume31612@gmail.com>
2020-04-09 17:20:28 +0900

    fix: remove unused navigation drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,5 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      v-navigation-drawer
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    />
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
